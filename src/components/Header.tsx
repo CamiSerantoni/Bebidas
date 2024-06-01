f8cdb9edef530be54334db510f28431c554c87ee
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-01 16:29:10 -0400

    mejora en header agregando navLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header() {
-  return (
- <header className='bg-slate-800' >
-    <div className='mx-auto container px-5 py-16'>
-        <div className='flex justify-between items-center'>
-            <div><img  className='w-32'    src="/logo.svg" alt="logotipo" /> </div>
-            <nav></nav>
+    return (
+        <header className='bg-slate-800' >
+            <div className='mx-auto container px-5 py-16'>
+                <div className='flex justify-between items-center'>
+                    <div><img className='w-32' src="/logo.svg" alt="logotipo" /> </div>
+                    <nav className='flex gap-4'>
+                        /* Si queremos resaltar pagina actual usamos navLink sino solo link */
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
 
-        </div>
-    </div>
- </header>
-  )
+                            }
+                        > Inicio</NavLink>
+                        <NavLink
+                            to="/favoritos"
+                            className={({isActive}) =>
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+
+                            }
+                        > Favoritos</NavLink>
+                    </nav>
+
+                </div>
+            </div>
+        </header>
+    )
 }
