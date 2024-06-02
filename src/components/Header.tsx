344cc075ddcbe89c777a4f14e3831f4ceba7c704
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-02 17:03:15 -0400

    se agrega img de fondo para inicio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   /* Si queremos resaltar pagina actual usamos navLink sino solo link */
 
   return (
-    <header className="bg-slate-800">
+    <header className={isHome ? 'bg-header bg-center bg-cover ': "bg-slate-800"}>
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
