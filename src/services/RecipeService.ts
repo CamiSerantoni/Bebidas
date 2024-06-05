bc4348e452fdc995d654e1771dc70d38fad4f037
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-05 09:26:20 -0400

    obteniendo categorias

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,3 +1,10 @@
+import axios from "axios";
+import { CategoriesAPIResponseSchema } from "../utils/recipes-schema";
+
+
 export async function getCategories() {
-  console.log('DESDE RECIPES SERVICES')
+const url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+  const  {data}  = await axios(url)
+ console.log("DESDE RECIPES SERVICES")
+ console.log(data)
 }
