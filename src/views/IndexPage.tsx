927576d328035d55000736244ede0dc9e711cf52
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-24 09:11:32 -0400

    validando existencia de resultados

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,26 @@
+
+import { useMemo } from "react";
+import { useAppStore } from "../stores/useAppStore";
+import { Drinks } from '../types/index';
+
+
 export default function IndexPage() {
+
+  const drinks =  useAppStore((state) => state.drinks)
+
+const hasDrinks = useMemo(() =>  drinks.drinks.length ,  [drinks])
+
+
   return (
     <>
-      <h1>Inicio</h1>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+      {hasDrinks ? (
+        <>   <p>  Si hay bebidas    </p>
+        </>)
+        : (
+          <>   <p className="my-10 text-center text-2xl">  No hay resultados aun, utiliza el formulario para buscar recetas    </p>
+          </>
+        )}
     </>
   );
 }
