eff715f16191e3d0c5fff64f498a4e175d6a78ee
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-24 08:42:01 -0400

    ejecutando respuestas de la consulta filtros ingrediente y categoria

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 import { CategoriesAPIResponseSchema } from "../utils/recipes-schema";
-
+import { SearchFilter } from "../types";
 
 export async function getCategories() {
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
-  const  { data }  = await axios(url)
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+  const { data } = await axios(url);
 
-  const result = CategoriesAPIResponseSchema.safeParse(data)
+  const result = CategoriesAPIResponseSchema.safeParse(data);
 
-
-if(result.success) {    
-    return result.data
+  if (result.success) {
+    return result.data;
+  }
 }
 
-//   console.log(result)
-//  console.log("DESDE RECIPES SERVICES")
-//  console.log(data)
+export async function getRecipes(filters: SearchFilter) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${filters.ingredient}&c=${filters.category}`;
+  const {data} = await axios(url)
+  console.log(data, 'DESDE RECIPES SERVICES the dataaaaaa')
 }
