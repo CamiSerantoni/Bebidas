e1e64ca954668a9c04ba6456d8c13a5246b9a5ce
Camila Serantoni <camila.serantoni@blue.cl>
2024-06-26 21:10:42 -0400

    se agregan clases card

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -7,12 +7,15 @@ type DrinkCardProps = {
 const DrinkCard = ({drink} :DrinkCardProps) => {
   return (
     <div className="border shadow-lg">
-      <div>
-<img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`} />
+      <div className="overflow-hidden">
+<img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`} className="hover:scale-125 transition-transform hover:rotate-2"/>
 
       </div>
+<div className="p-5">
+  <h2 className="text-2xl truncate font-black ">{drink.strDrink}</h2>
 
-      <h2>{drink.strDrink}</h2>
+</div>
+    
     </div>
   )
 }
