5ee8615a6d954363ca13dcde9f40e131e08842f2
Camila Serantoni <camila.serantoni@blue.cl>
2024-07-16 12:50:12 -0400

    seleccionando favorito

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -3,13 +3,18 @@ import { Recipe } from '../types'
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
+    handleClickFavorite: (recipe: Recipe) => void
 }
 
 export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
   favorites: [],
+  handleClickFavorite: (recipe) => {
+    console.log(recipe)
+  },
+
 //   addFavorite: (recipe: Recipe) => set((state) => ({ favorites: [...state.favorites, recipe] })),
 //   removeFavorite: (recipe: Recipe) => set((state) => ({ favorites: state.favorites.filter((f) => f.idDrink !== recipe.idDrink) })),
 })
 
 
-//El dividir los slices  se le conoce como "Slice pattern"
+//El dividir los slices  se le conoce como el patrón:  "Slice pattern" permite tener codigo más organizado 
