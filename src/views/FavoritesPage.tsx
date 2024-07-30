8d64e3e4cd9631e16c602a9e3debc0937b1c615b
Camila Serantoni <camila.serantoni@blue.cl>
2024-07-30 07:50:19 -0400

    primeros pasos con favoritos se visualizan y muestran condicionalmente

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,34 @@
+
 import React from 'react'
+import { useMemo } from 'react'
+import { useAppStore } from '../stores/useAppStore'
+import DrinkCard from '../components/DrinkCard'
 
 export const FavoritesPage = () => {
+
+const favorites = useAppStore((state) => state.favorites)
+const hasFavorites = useMemo(() => favorites.length, [favorites])
+
+
   return (
-    <div>FavoritesPage</div>
+<>
+<h1 className='text-6xl font-extrabold' > Favoritos</h1>
+
+{/* <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 my-10  mx-10  gap-10 md:gap-20"> */}
+
+{
+  hasFavorites ? (
+    <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 my-10  mx-10  gap-10 md:gap-20">
+  {favorites.map((drink) => (
+    <DrinkCard key={drink.idDrink} drink={
+      drink}/>
+  ))}
+</div>
+  ) : (
+
+    <p className='my-10 text-center text-2xl'> Los favoritos se mostrarán aquí</p>
+  )
+}
+</>
   )
 }
